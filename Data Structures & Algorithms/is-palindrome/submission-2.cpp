8ff8593aa662d617cd50eeaf2af924e7c143097a
Ketan Bhadwariya <82806574+ketoin23@users.noreply.github.com>
2026-09-11 22:44:14 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    bool check(char c) {
+        if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+            return true;
+
+        return false;
+    }
+public:
+    bool isPalindrome(string s) {
+        for(int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            while(i < j && !check(s[i]))
+                ++i;
+            while(i < j && !check(s[j]))
+                --j;
+
+            if(s[i] <= '9' && s[i] >= '0') {
+                if(s[i] != s[j])
+                    return false;
+            } else {
+                if(s[i] == s[j] || toupper(s[i]) == s[j] || tolower(s[i]) == s[j])
+                    continue;
+                
+                cout << s[i] << " " << s[j];
+                
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
